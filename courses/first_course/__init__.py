8a8c357ae8e54ad10b97cf2d84961e7eadb4d494
Zachary B. Mott <zmott@nerdery.com>
2018-08-22 09:30:12 -0500

    Uncomment all the other holes in first_course.

--- courses/first_course/__init__.py
+++ courses/first_course/__init__.py
@@ -8,14 +8,14 @@ __all__ = ['course']
 
 
 course = Course('The First Course', [
-    # 'courses.first_course.Hole1',
-    # 'courses.first_course.Hole2',
-    # 'courses.first_course.Hole3',
-    # 'courses.first_course.Hole4',
-    # 'courses.first_course.Hole5',
-    # 'courses.first_course.Hole6',
-    # 'courses.first_course.Hole7',
-    # 'courses.first_course.Hole8',
-    # 'courses.first_course.Hole9',
+    'courses.first_course.Hole1',
+    'courses.first_course.Hole2',
+    'courses.first_course.Hole3',
+    'courses.first_course.Hole4',
+    'courses.first_course.Hole5',
+    'courses.first_course.Hole6',
+    'courses.first_course.Hole7',
+    'courses.first_course.Hole8',
+    'courses.first_course.Hole9',
     'courses.first_course.Hole10',
 ])
